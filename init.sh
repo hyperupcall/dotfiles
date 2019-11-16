ad2385b0486f2f21bc8c1b0b8f033abc488fa64b
eankeen <24364012+eankeen@users.noreply.github.com>
2019-11-16 00:12:03 -0800

    feat: add git configs also add in stow commands to stow most of config

--- init.sh
+++ init.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 . init/remove-existing-dotfiles.sh
 . init/xdg-user-dirs.sh
 
 # stow
-stow bash
-
-# git
-git config --global user.name "eankeen"
-git config --global user.email "24364012+eankeen@users.noreply.github.com"
-git config --global core.editor vim
-
+stow -S bash
+stow -S git
+stow -S vscode
+stow -S vim
