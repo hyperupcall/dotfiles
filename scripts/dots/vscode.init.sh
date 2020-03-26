5178c1fb39dd382e16fd0f90f7ec6a29fe716017
eankeen <24364012+eankeen@users.noreply.github.com>
2020-03-26 00:08:36 -0700

    refactor: vscode.init.sh to install extensions from arguments can install extensions found in a vscode extensions directory or extensions listed by vscode itself

--- scripts/dots/vscode.init.sh
+++ scripts/dots/vscode.init.sh
@@ -1,48 +1,61 @@
-code --install-extension arcticicestudio.nord-visual-studio-code
-code --install-extension bierner.lit-html
-code --install-extension bungcip.better-toml
-code --install-extension christian-kohler.npm-intellisense
-code --install-extension cpylua.language-postcss
-code --install-extension davidanson.vscode-markdownlint
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension donjayamanne.githistory
-code --install-extension donjayamanne.githistory
-code --install-extension eamodio.gitlens
-code --install-extension ecmel.vscode-html-css
-code --install-extension editorconfig.editorconfig
-code --install-extension eg2.vscode-npm-script
-code --install-extension esbenp.prettier-vscode
-code --install-extension esbenp.prettier-vscode
-code --install-extension firefox-devtools.vscode-firefox-debug
-code --install-extension formulahendry.auto-close-tag
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension gogocrow.webpack-snippets
-code --install-extension hollowtree.vue-snippets
-code --install-extension jamesbirtles.svelte-vscode
-code --install-extension jasonnutter.search-node-modules
-code --install-extension mauve.terraform
-code --install-extension mgmcdermott.vscode-language-babel
-code --install-extension mrcrowl.hg
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension ms-mssql.mssql
-code --install-extension ms-python.python
-code --install-extension ms-vscode.cpptools
-code --install-extension ms-vscode.go
-code --install-extension octref.vetur
-code --install-extension pkief.material-icon-theme
-code --install-extension pnp.polacode
-code --install-extension redhat.vscode-yaml
-code --install-extension richie5um2.vscode-sort-json
-code --install-extension shigma.vscode-wl
-code --install-extension shinnn.stylelint
-code --install-extension tanato.vscode-gulp
-code --install-extension timonwong.shellcheck
-code --install-extension tyriar.sort-lines
-code --install-extension vsciot-vscode.vscode-arduino
-code --install-extension vscodevim.vim
-code --install-extension xabikos.javascriptsnippets
-code --install-extension zignd.html-css-class-completion
+#!/bin/sh
+
+help() {
+  echo "batch install plugins for vscode"
+  echo
+  echo "vscode.init.sh 'list of extensions'"
+  echo
+  echo "flags"
+  echo "  --help              display this help menu"
+  echo "  --remove-versions    remove versions from extensions"
+  echo
+  echo "examples"
+  echo "  vscode.init.sh --remove-versions \"\$(ls ~/.vscode/extensions)\""
+  echo "  vscode.init.sh \"\$(code --list-extensions)\""
+}
+
+check_empty() {
+  if [ -z "$1" ] || [ -z "$(echo "$1" | sed "s/ //g" | sed "s/\t//g")" ]
+  then
+    echo "list of extensions is blank. terminating."
+    exit 1
+  fi
+}
+
+if [ "$#" -eq 0 ] || [ "$#" -ge 3 ]
+then
+  help
+  exit 1
+fi
+
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]
+then
+  help
+  exit
+fi
+
+if [ "$1" = "--remove-versions" ]
+then
+  check_empty "$2"
+  for ext in $2
+  do
+    code --install-extension "${ext%-*}"
+  done
+  exit
+fi
+
+if [ "$2" = "--remove-versions" ]
+then
+  check_empty "$1"
+  for ext in $1
+  do
+    code --install-extension "${ext%-*}"
+  done  
+  exit
+fi
+
+check_empty "$1"
+for ext in $1
+do
+  code --install-extension "$ext"
+done 
