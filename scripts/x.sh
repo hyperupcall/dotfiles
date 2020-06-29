6880439171e36b1ce42d419733477523e4692b28
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-29 16:52:30 -0700

    small changes

--- scripts/x.sh
+++ scripts/x.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -eu
 
-function primary {
+primary() {
 	xrandr \
-		--output DP-5 --auto \
+		--output DP-5 --above DP-3 --auto \
 		--output DP-3 --below DP-3 --auto \
 		--output DP-3 --left-of HDMI-0 --auto \
 		--output DVI-D-0 --right-of HDMI-0 --rotate left --auto
