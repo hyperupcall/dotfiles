355ed269d5db233015993b9935d0563ac1409261
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-29 17:23:08 -0700

    feat: add refind theme regular

---
+++ global/efi/EFI/refind/refind-theme-regular/install.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+# An installer for refind-theme-regular by Munlik
+
+#Check if root
+[[ $EUID -ne 0 ]] && echo "This script must be run as root." && exit 1
+
+clear
+
+#Clone the theme
+echo -n "Downloading rEFInd theme Regular to $PWD"
+git clone https://github.com/bobafetthotmail/refind-theme-regular.git &> /dev/null
+echo " - [DONE]"
+
+#Useful formatting tags
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+#Set install path
+echo "Enter rEFInd install location"
+read -e -p "Default - ${bold}/boot/efi/EFI/refind/${normal}: " location
+if test -z "$location";
+then
+    location="/boot/efi/EFI/refind/"
+fi
+if test "${location: -1}" != "/"
+then
+    location="$location/"
+fi
+
+#Set icon size
+echo "Pick an icon size: (larger icons look better on bigger and denser displays)"
+read -p "${bold}1: small (128px-48px)${normal}, 2: medium (256px-96px), 3: large (384px-144px), 4: extra-large (512px-192px): " size_select
+if test -z "$size_select";
+then
+    size_select=1
+fi
+case "$size_select" in
+    1)
+        size_big="128"
+        size_small="48"
+        ;;
+    2)
+        size_big="256"
+        size_small="96"
+        ;;
+    3)
+        size_big="384"
+        size_small="144"
+        ;;
+    4)
+        size_big="512"
+        size_small="192"
+        ;;
+    *)
+        echo "Incorrect choice. Exiting."
+        exit 1
+        ;;
+esac
+echo
+echo "Selected size - ${bold}big icons: $size_big px, small icons: $size_small px${normal}"
+echo
+
+#Set theme color
+echo "Select a theme color"
+read -p "${bold}1: light${normal}, 2: dark: " theme_select
+if test -z "$theme_select";
+then
+    theme_select=1
+fi
+case "$theme_select" in
+    1)
+        theme_name="light"
+        theme_path=""
+        ;;
+    2)
+        theme_name="dark"
+        theme_path="_dark"
+        ;;
+    *)
+        echo "Incorrect choice. Exiting."
+        exit 1
+        ;;
+esac
+echo
+echo "Selected theme - ${bold}$theme_name${normal}"
+echo
+#Uncomment relevant lines from src/theme.conf
+echo -n "Generating theme file theme.conf"
+cd refind-theme-regular
+cp src/theme.conf theme.conf
+sed -i "s/#icons_dir refind-theme-regular\/icons\/$size_big-$size_small/icons_dir refind-theme-regular\/icons\/$size_big-$size_small/" theme.conf
+sed -i "s/#big_icon_size $size_big/big_icon_size $size_big/" theme.conf
+sed -i "s/#small_icon_size $size_small/small_icon_size $size_small/" theme.conf
+sed -i "s/#banner refind-theme-regular\/icons\/$size_big-$size_small\/bg$theme_path.png/banner refind-theme-regular\/icons\/$size_big-$size_small\/bg$theme_path.png/" theme.conf
+sed -i "s/#selection_big refind-theme-regular\/icons\/$size_big-$size_small\/selection$theme_path-big.png/selection_big refind-theme-regular\/icons\/$size_big-$size_small\/selection$theme_path-big.png/" theme.conf
+sed -i "s/#selection_small refind-theme-regular\/icons\/$size_big-$size_small\/selection$theme_path-small.png/selection_small refind-theme-regular\/icons\/$size_big-$size_small\/selection$theme_path-small.png/" theme.conf
+cd ..
+echo " - [DONE]"
+
+#Clean up
+echo -n "Removing unused directories"
+rm -rf refind-theme-regular/{src,.git}
+rm -rf refind-theme-regular/install.sh
+echo " - [DONE]"
+
+#Remove previous installs
+echo -n "Deleting older installed versions (if any)"
+rm -rf "$location"{regular-theme,refind-theme-regular}
+echo " - [DONE]"
+
+#Copy theme setup folders
+echo -n "Copying theme to $location"
+cp -r refind-theme-regular "$location"
+echo " - [DONE]"
+
+#Edit refind.conf - remove older themes
+echo -n "Removing old themes from refind.conf"
+echo
+echo
+read -p "Do you have a secondary config file to preserve? Default: N (y/${bold}N${normal}): " config_confirm
+if test -z "$config_confirm";
+then
+    config_confirm="n"
+fi
+case "$config_confirm" in
+    y|Y)
+        read -p "Enter the name of the config file to be preserved in full eg: manual.conf: " configname
+        # Checking for enter key. If so it has the same effect having no files to preserve.
+        if [[ $configname == "" ]]; then
+	configname='^#'
+	fi
+        #Excludes line with entered config file then ^\s*include matches lines starting with any nuber of spaces and then include.
+        sed --in-place=".bak" "/$configname/! s/^\s*include/# (disabled) include/" "$location"refind.conf
+        ;;
+    n|N)
+        # ^\s*include matches lines starting with any nuber of spaces and then include.
+        sed --in-place=".bak" 's/^\s*include/# (disabled) include/' "$location"refind.conf
+        ;;
+    *)
+        ;;
+esac
+echo " - [DONE]"
+
+#Edit refind.conf - add new theme
+echo -n "Updating refind.conf"
+echo "
+# Load rEFInd theme Regular
+include refind-theme-regular/theme.conf" | tee -a "$location"refind.conf &> /dev/null
+echo " - [DONE]"
+
+#Clean up - remove download
+read -p "Delete download? (${bold}Y${normal}/n): " del_confirm
+if test -z "$del_confirm";
+then
+    del_confirm="y"
+fi
+case "$del_confirm" in
+    y|Y)
+        echo -n "Deleting download"
+        rm -r refind-theme-regular
+        echo " - [DONE]"
+        ;;
+    *)
+        ;;
+esac
+
+echo "Thank you for installing rEFInd theme Regular."
+echo "NOTE: If you're not getting your full resolution or have color issues then try disabling the CSM in your UEFI settings."
---
+++ global/efi/EFI/refind/refind-theme-regular/src/render_bitmap.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+
+INKSCAPE=`which inkscape 2> /dev/null`
+OPTIPNG=`which optipng 2> /dev/null`
+
+OUT="bitmap"
+BIG_ICON_SRC_DIR="svg/big"
+SMALL_ICON_SRC_DIR="svg/small"
+REG_BIG_ICON_FILENAME='s/^svg\/big\///g;s/.svg$//g'
+REG_SMALL_ICON_FILENAME='s/^svg\/small\///g;s/.svg$//g'
+DPI=96
+SCALE_PRESET=(1 2 3 4)
+SCALE=1
+SRC_DIR=""
+OUT_DIR=""
+REG=''
+
+function render_bitmap(){
+    for svgfile in $(ls $SRC_DIR | grep .svg)
+        do
+            filename=$(echo $svgfile | sed $REG)
+            if [ -f "$OUT_DIR/$filename.png" ]
+                then
+                    echo "'$OUT_DIR/$filename.png' already exists"
+                else
+                    echo "Creating... $OUT_DIR/$filename.png"
+                	$INKSCAPE --export-area-page \
+                              --export-dpi=$(($SCALE*$DPI)) \
+                              --export-png="$OUT_DIR/$filename.png" $SRC_DIR/$svgfile> /dev/null \
+                    &&
+                    if [[ -x $OPTIPNG ]]
+                        then
+                            $OPTIPNG -o7 --quiet "$OUT_DIR/$filename.png"
+                    fi
+
+            fi
+        done
+
+        if [ -f $OUT_DIR/os_unknown.png ]
+            then
+                for f in os_clover os_gummiboot os_hwtest os_refit os_network os_systemd-boot
+                    do
+                        echo "Copying... $OUT_DIR/$f.png"
+                        cp -f "$OUT_DIR/os_unknown.png" "$OUT_DIR/$f.png"
+                done
+     	fi
+
+        if [ -f $OUT_DIR/tool_rescue.png ]
+            then
+                for f in tool_apple_rescue tool_windows_rescue
+                    do
+                        echo "Copying... $OUT_DIR/$f.png"
+                        cp -f "$OUT_DIR/tool_rescue.png" "$OUT_DIR/$f.png"
+                done
+
+        fi
+}
+function render_big_icon(){
+    SCALE=$i
+    REG=$REG_BIG_ICON_FILENAME
+    SRC_DIR=$BIG_ICON_SRC_DIR
+    OUT_DIR="$OUT/big_$(($i*128))"
+    mkdir -p $OUT_DIR
+    render_bitmap
+}
+function render_small_icon(){
+    SCALE=$i
+    REG=$REG_SMALL_ICON_FILENAME
+    SRC_DIR=$SMALL_ICON_SRC_DIR
+    OUT_DIR="$OUT/small_$(($i*48))"
+    mkdir -p $OUT_DIR
+    render_bitmap
+}
+
+for i in ${SCALE_PRESET[@]}
+   do
+        render_big_icon
+   	    render_small_icon
+done
+exit 0
