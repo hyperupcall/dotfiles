9b4d6f7b0f3a36b70167b9e897b09ce223ebbe8a
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2020-08-11 01:29:43 -0700

    fix: adapt x script for new monitor setup

--- scripts/x.sh
+++ scripts/x.sh
@@ -3,7 +3,8 @@
 primary() {
 	xrandr \
 		--output DP-5 --above DP-3 --auto \
-		--output DP-3 --below DP-3 --auto \
-		--output DP-3 --left-of HDMI-0 --auto \
-		--output DVI-D-0 --right-of HDMI-0 --rotate left --auto
+		--output DP-3 --below DP-5 --auto \
+		--output DP-5 --left-of HDMI-0 --auto \
+		--output DP-1 --right-of DP-3 --auto
+#		--output DP-1 --right-of HDMI-0 --rotate left --auto
 }
