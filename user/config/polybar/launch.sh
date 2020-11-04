d1dcb021274c2e6f0124ff1efbaf55508dc73d76
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2020-11-03 16:23:01 -0800

    add polybar dotfiles

--- user/config/polybar/launch.sh
+++ user/config/polybar/launch.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-# Terminate already running bar instances
 killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u 1000 -x polybar >/dev/null; do sleep 1; done
-
-polybar example &
-
-
+# while pgrep -x polybar >/dev/null; do sleep 1; done
+echo foo
+bar="main"
+if type "xrandr" >/dev/null 2>&1; then
+	for m in $(xrandr --query | grep ' connected ' | cut -d' ' -f1); do
+		MONITOR=$m polybar --reload "$bar" &
+	done
+else
+	echo "Error: xrandr not found" 1>&2
+	polybar --reload "$bar" &
+fi
