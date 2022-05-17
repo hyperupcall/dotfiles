377b6778ef41fdfde764ec149133dfc7b8342bb2
Edwin Kofler <edwin@kofler.dev>
2022-05-16 17:53:18 -0700

    lint: Add lint ignore

--- bootstrap/dotmgr/src/commands/action.sh
+++ bootstrap/dotmgr/src/commands/action.sh
@@ -123,6 +123,7 @@ subcommand() {
 			exit
 			;;
 		1|2|3|4|5|6|7|8|9)
+			# shellcheck disable=SC1007
 			local -i i= adjustedi=-1
 			for ((i=0; i < key; ++i)); do
 				if [ -z "${files[$i]}" ]; then
