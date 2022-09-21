d54bfb684e0e05b812e95dc74a6b7c2730f0fe91
Edwin Kofler <edwin@kofler.dev>
2022-09-21 13:55:51 -0700

    Update: September 21, 2022 (13:55)

--- dotmgr/actions-plumbing/42_install_ghc.sh
+++ dotmgr/actions-plumbing/42_install_ghc.sh
@@ -3,17 +3,18 @@
 # Name:
 # Install GHC
 
-if util.confirm 'Install GHC?'; then
-	core.print_info "Not implemented" # TODO (fully automate)
-	# if ! command -v haskell >/dev/null 2>&1; then
-	# 	core.print_info "Installing haskell"
+main() {
+	if util.confirm 'Install GHC?'; then
+		if ! command -v haskell >/dev/null 2>&1; then
+			core.print_info "Installing haskell"
 
-	# 	mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}/ghcup"
-	# 	ln -s "${XDG_DATA_HOME:-$HOME/.local/share}"/{,ghcup/.}ghcup
+			mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}/ghcup"
+			ln -s "${XDG_DATA_HOME:-$HOME/.local/share}"/{,ghcup/.}ghcup
 
-	# 	util.req https://get-ghcup.haskell.org | sh
+			util.req https://get-ghcup.haskell.org | sh
 
-	# 	util.req https://get.haskellstack.org/ | sh
-	# fi
-	:
-fi
+			util.req https://get.haskellstack.org/ | sh
+		fi
+	fi
+}
+
