1fe066604abb08b09729d8b90ba3cc47736a10f3
Edwin Kofler <edwin@kofler.dev>
2022-09-22 10:03:53 -0700

    Update: September 22, 2022 (10:03)

--- dotmgr/actions-plumbing/52_install_dropbox.sh
+++ dotmgr/actions-plumbing/52_install_dropbox.sh
@@ -5,17 +5,17 @@ main() {
 		util.cd_temp
 
 		core.print_info 'Downloading'
-		util.req -o ./dropbox.tar.gz "https://www.dropbox.com/download?plat=lnx.x86_64"
+		util.req --http1.1 -o ./dropbox.tar.gz "https://www.dropbox.com/download?plat=lnx.x86_64" || util.die
 
 		core.print_info 'Extracting'
-		tar xzf ./dropbox.tar.gz
+		tar xzf ./dropbox.tar.gz || util.die
 
 		core.print_info 'Copying'
-		rm -rf ~/.dots/.home/Downloads/.dropbox-dist
-		mv ./.dropbox-dist ~/.dots/.home/Downloads
+		rm -rf ~/.dots/.home/Downloads/.dropbox-dist || util.die
+		mv ./.dropbox-dist ~/.dots/.home/Downloads || util.die
 
 		core.print_info 'Symlinking'
-		ln -sf ~/.dots/.home/Downloads/.dropbox-dist/dropboxd ~/.dots/.usr/bin/dropboxd
+		ln -sf ~/.dots/.home/Downloads/.dropbox-dist/dropboxd ~/.dots/.usr/bin/dropboxd || util.die
 
 		popd >/dev/null
 	fi
