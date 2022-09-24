ef4aa8bae28078cbca31608753b1c0bea4d5c315
Edwin Kofler <edwin@kofler.dev>
2022-09-23 21:55:13 -0700

    Update: September 23, 2022 (21:55)

---
+++ dotmgr/actions/13_doctor.sh
@@ -0,0 +1,7 @@
+# shellcheck shell=bash
+
+main() {
+	# Here, test if various processes and daemons are running like they are supposed to
+	# Maybe verify if certain shortcuts exist or certain binaries are in the PATH (of either interactive or non-interactive apps)
+	printf '%s\n' 'Not implemented'
+}
