28cbcda817634993c507631fe2f75e82b30fa4c0
Edwin Kofler <edwin@kofler.dev>
2022-10-08 10:27:33 -0700

    update: October 08, 2022 (10:27)

--- user/.config/shell/modules/functions/apps.sh
+++ user/.config/shell/modules/functions/apps.sh
@@ -4,6 +4,6 @@ foxxo() {
 	deno run --unstable --allow-run --allow-env --allow-read --allow-write --allow-net "$HOME/repos/foxxo/output/bundle.js" "$@"
 }
 
-foxxo-dev() {
+foxxo_dev() {
 	deno run --unstable --allow-run --allow-env --allow-read --allow-write --allow-net "$HOME/repos/foxxo/src/main.ts" "$@"
 }
