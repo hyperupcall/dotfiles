b1c1f21db77be756c7ba06eed3067180b619bf83
Edwin Kofler <edwin@kofler.dev>
2023-01-19 23:31:04 -0800

    update: January 19, 2023 (23:31)

--- Bakefile.sh
+++ Bakefile.sh
@@ -5,7 +5,11 @@ task.init() {
 	git config --local filter.npmrc-clean.clean "$PWD/user/.config/npm/npmrc-clean.sh"
 	git config --local filter.slack-term-config-clean.clean "$PWD/user/.config/slack-term/slack-term-config-clean.sh"
 	git config --local filter.oscrc-clean.clean "$PWD/user/.config/osc/oscrc-clean.sh"
-	hookah refresh
+	if command -v hookah &>/dev/null; then
+		hookah refresh
+	else
+		bake.info 'Skipping hookah'
+	fi
 }
 
 task.build() {
@@ -14,14 +18,10 @@ task.build() {
 	sed 's/XTerm/UXTerm/g' xterm.Xresources >> uxterm.Xresources
 }
 
-task.update-subtree() {
-	git subtree -P vendor/bats-all --squash pull 'https://github.com/hyperupcall/bats-all' HEAD
-}
-
 task.test() {
 	cd "./os/*nix/user/.config/shell/modules/common" && bats -p .
 }
 
 task.commit() {
-	git commit -m "update: $(date "+%B %d, %Y (%H:%M)")" "$@"
+	git commit -m "update: $(date '+%B %d, %Y (%H:%M)')" "$@"
 }
