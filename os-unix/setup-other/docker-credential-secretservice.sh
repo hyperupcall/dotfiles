c65477579168b8dad8b2dcc1483812eed385f481
Edwin Kofler <edwin@kofler.dev>
2025-08-29 16:03:52 -0700

    2025.08.29

--- os-unix/setup-other/docker-credential-secretservice.sh
+++ os-unix/setup-other/docker-credential-secretservice.sh
@@ -2,13 +2,20 @@
 
 source ~/.dotfiles/os-unix/data/source.sh
 
-declare -g g_name='docker secretservice credential store version v0.6.4'
+declare -g g_name='docker secretservice credential store'
 
 main() {
-	local version='v0.6.4'
-	curl -K "$CURL_CONFIG" -o 'docker-credential-secretservice.tar.gz' "https://github.com/docker/docker-credential-helpers/releases/download/$version/docker-credential-secretservice-$version-amd64.tar.gz"
-	tar xf 'docker-credential-secretservice.tar.gz'
-	mv './docker-credential-secretservice' "$HOME/bin"
+	util.get_latest_github_tag 'docker/docker-credential-helpers'
+	local version="$REPLY"
+	echo "$version"
+	curl -K "$CURL_CONFIG" -o 'docker-credential-secretservice' "https://github.com/docker/docker-credential-helpers/releases/download/$version/docker-credential-secretservice-$version.linux-amd64"
+	chmod +x './docker-credential-secretservice'
+	mv './docker-credential-secretservice' "$HOME/.local/bin"
+
+	if [ ! -f "$XDG_CONFIG_HOME/docker/config.json" ]; then
+		mkdir -p "$XDG_CONFIG_HOME/docker"
+		printf '%s\n' '{}' > "$XDG_CONFIG_HOME/docker/config.json"
+	fi
 
 	python -c "import json
 import os
@@ -22,7 +29,7 @@ file.write_text(json.dumps(obj, indent='\t'))"
 }
 
 installed() {
-	[ -f ~/bin/docker-credential-secretservice ]
+	[ -f "$HOME/.local/bin/docker-credential-secretservice" ]
 }
 
 util.if_file_sourced || _setup "$@"
